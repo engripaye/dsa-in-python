75a682b619b4d6024a409a403dc3256f311adf02
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-07 04:02:53 +0100

    initial commit

---
+++ practice.py
@@ -0,0 +1,21 @@
+# LIST
+toys = ["car", "doll", "ball"]
+print(toys[1])
+
+# DICTIONARY
+toy_box = {"red": "car", "blue":"doll"}
+print(toy_box["red"])
+
+# SET
+unique_toy = {"car", "doll", "ball", "train"}
+print(unique_toy)
+
+# TUPLE
+fixed_toys = ("car", "doll", "ball")
+print(fixed_toys[1])
+
+# ALGORITHM - are recipes for solving a problem e.g you want to 
+# find a favorite toy in a list 
+# Linear Search - check each item one by one
+# Binary Search - cut the list in half each time  ---- (requires sorted list)
+

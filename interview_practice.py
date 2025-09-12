45c0d75f50646eb2aebcb1fbd0273bd2f534e7be
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-12 11:00:49 +0100

    updated dsa code

---
+++ interview_practice.py
@@ -0,0 +1,71 @@
+# REVERSE A STRING 
+def reverse_string(s):
+    return s[::-1]
+
+print(reverse_string("hello"))
+
+def reverse_string2(m):
+    return m[::-1]
+
+print(reverse_string2("davido"))
+
+
+# CHECK FOR PALINDROME -- A palindrome checks if a string read thesame backward and forward
+def check_palindrome(s):
+    return s == s[::-1]
+
+print(check_palindrome("madam"))
+print(check_palindrome("davido"))
+
+
+# CHECK FOR MISSING NUMBER
+def missing_number(nums):
+    n = len(nums) + 1
+    expected_sum = n * (n + 1) // 2
+    return expected_sum - sum(nums)
+
+print(missing_number([1, 2, 4, 5]))
+
+
+# CHECK DUPLICATES 
+def find_duplicates(nums):
+    seen = set()
+    duplicates = []
+    for num in nums:
+        if num in seen:
+            duplicates.append(num)
+        else:
+            seen.add(num)
+    return duplicates
+
+print(find_duplicates([1, 2, 3, 4, 1, 4]))
+
+# TWO SUM PROBLEMS
+def two_sum_pro(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return [seen[diff], i]
+        seen[num] = i
+    return []
+
+print(two_sum_pro([1, 2, 3, 4], 3))     
+
+
+# MERGE TWO SORTED LIST
+def merge_sorted_list(li, l2):
+    result = []
+    i = j = 0
+    while i < len(li) and j < len(l2):
+        if li[i] < l2[j]:
+            result.append(li[i])
+            i += 1
+        else:
+            result.append(l2[j])
+            j += 1
+    result.extend(li[i:])
+    result.extend(l2[j:]) 
+    return result
+
+print(merge_sorted_list([1, 3, 5], [2, 4, 6]))               
